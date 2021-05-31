6fab7b48176db525b6143529648bb35a73c0bdcd
qiikaii <sharpnshiny22@gmail.com>
2021-05-31 15:28:40 +0800

    Create shadowingstatetvariables_fixed.sol

---
+++ shadowingstatetvariables_fixed.sol
@@ -0,0 +1,29 @@
+pragma solidity 0.5.12;
+
+//We fix the problem by eliminating the declaration which overrides the prefered hardcap.
+
+contract Tokensale {
+    uint public hardcap = 10000 ether;
+
+    // function Tokensale() {}
+    constructor() public {}
+
+    // function fetchCap() public view returns(uint) {
+    //     return hardcap;
+    // }
+    
+    function fetchCap() external view returns(uint) {
+        return hardcap;
+    }
+}
+
+contract Presale is Tokensale {
+    //uint hardcap = 1000 ether;
+    //If the hardcap variables were both needed we would have to rename one to fix this.
+    constructor() public {
+        hardcap = 1000 ether;
+    }
+    // function Presale() Tokensale() {
+    //     hardcap = 1000 ether; //We set the hardcap from the constructor for the Tokensale to be 1000 instead of 10000
+    // }
+}
