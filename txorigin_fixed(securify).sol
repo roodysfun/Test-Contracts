7a660d0ca66e26ea0235334b224143d862cd8c8e
qiikaii <sharpnshiny22@gmail.com>
2021-06-02 19:43:42 +0800

    Upload txorigin_fixed(securify).sol

---
+++ txorigin_fixed(securify).sol
@@ -0,0 +1,17 @@
+
+
+// THIS CONTRACT CONTAINS A BUG - DO NOT USE
+contract TxUserWallet {
+    address public owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    function transferTo(address payable dest, uint amount) external {
+        require(address(dest) != address(0));
+	require(amount >= 0);
+    	require(msg.sender == owner);
+        dest.transfer(amount);
+    }
+}
